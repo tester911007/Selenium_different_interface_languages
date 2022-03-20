1d4b8280865fe470510250f5f964deaa60b824e2
tester911007 <60608141+tester911007@users.noreply.github.com>
2022-03-20 12:31:07 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -8,4 +8,4 @@ class TestProductPage:
     def test_presence_button_add_to_basket(self, browser):
         browser.get(link)
         time.sleep(30)
-        assert browser.find_element_by_css_selector("button.btn-add-to-basket")
+        assert browser.find_element_by_css_selector("button.btn-add-to-basket"), 'Кнопка не найдена'
